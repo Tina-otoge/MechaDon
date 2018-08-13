25ea06c0063c3fb786aa9a45d7778351d1c66e87
Tina <skielred@gmail.com>
2018-08-13 02:30:08 +0200

    Augments DBInterface with set_by_id and commit
    - Create helper static method list_to_sql and uses it in get_by_id and
    set_by_id functions
    - Some code cleaning in the DBInterface class

--- mechadon/dbinterface.py
+++ mechadon/dbinterface.py
@@ -5,7 +5,10 @@ class DBInterface():
     db_path = None
 
     def __init__(self):
-        self.connection = sqlite3.connect(self.db_path)
+        connection             = sqlite3.connect(self.db_path)
+        connection.row_factory = sqlite3.Row
+
+        self.connection = connection
         self.cursor     = self.connection.cursor()
 
     def __enter__(self):
@@ -14,17 +17,35 @@ class DBInterface():
     def __exit__(self, exception_type, exception_value, traceback):
         self.connection.close()
 
-    def execute(self, sql):
-        self.cursor.executescript(sql)
+    @staticmethod
+    def list_to_sql(list_, setter=False):
+        format_ = '`{}`'
 
-    def get_by_id(table=None, id_, columns='*'):
-        if table is None:
-            raise ValueError('table can not be None')
+        if setter:
+            format_ += '=?'
 
+        return ', '.join(format_.format(element) for element in list_)
+
+    def execute(self, script):
+        self.cursor.executescript(script)
+
+    def commit(self):
+        self.connection.commit()
+
+    def get_by_id(self, table, id_, columns='*'):
         if columns != '*':
-            columns = ', '.join('`{}`'.format(column) for column in columns)
+            columns = DBInterface.list_to_sql(columns)
 
         sql = 'SELECT {} FROM {} WHERE `id`=?'.format(columns, table)
-        self.cursor.execute(sql, tuple(id_))
+        self.cursor.execute(sql, (id_,))
 
         return self.cursor.fetchone()
+
+    def set_by_id(self, table, id_, dictionary):
+        sql = 'UPDATE {} SET {} WHERE `id`=?'.format(
+                table, DBInterface.list_to_sql(dictionary, setter=True))
+
+        values = list(dictionary.values())
+        values.append(id_)
+
+        return self.cursor.execute(sql, tuple(values))
